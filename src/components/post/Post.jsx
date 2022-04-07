8d3c3c193f21722b578eb6973c46f204e56e4fc3
Thomas Regourd <tom.reg@hotmail.fr>
2022-04-07 19:40:09 +0200

    (main) : initial version - tutorial 1:31 - dynamic posts

---
+++ src/components/post/Post.jsx
@@ -0,0 +1,43 @@
+import "./post.css";
+import { MoreVert } from "@mui/icons-material";
+import { Posts, Users } from "../../dummyData";
+import { useState } from "react";
+
+export default function Post({ post }) {
+  const findUserId = Users.filter((user) => user.id === post.userId);
+
+  return (
+    <div className="post">
+      <div className="postWrapper">
+        <div className="postTop">
+          <div className="postTopLeft">
+            <img
+              className="postProfileImg"
+              src={findUserId[0].profilePicture}
+              alt=""
+            />
+            <span className="postUsername">{findUserId[0].username}</span>
+            <span className="postDate">{post.date}</span>
+          </div>
+          <div className="postTopRight">
+            <MoreVert />
+          </div>
+        </div>
+        <div className="postCenter">
+          <span className="postText">{post?.desc}</span>
+          <img className="postImg" src={post.photo} alt="" />
+        </div>
+        <div className="postBottom">
+          <div className="postBottomLeft">
+            <img className="likeIcon" src="assets/like.png" alt="" />
+            <img className="likeIcon" src="assets/heart.png" alt="" />
+            <span className="postLikeCounter">{post.like} people like it</span>
+          </div>
+          <div className="postBottomRight">
+            <span className="postCommentText">{post.comment} comments</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
