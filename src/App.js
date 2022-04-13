a61b67e27cb88dc541d9bacee5ee83406fb53feb
Thomas Regourd <tom.reg@hotmail.fr>
2022-04-13 08:38:03 +0200

    (feat) : login page added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Home from "./pages/home/Home";
 import "../src/App.css";
 import Profile from "./pages/profile/Profile";
+import Login from "./pages/login/Login";
 // import Login from "./pages/login/Login";
 // import Profile from "./pages/profile/Profile";
 // import Register from "./pages/register/Register";
 
 function App() {
-  return <Home />;
+  return <Login />;
 }
 
 export default App;
